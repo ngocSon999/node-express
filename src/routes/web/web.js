ed16c63998871b6089fb5d7c01c15ca5d9f63d45
ngocthat <ntsh9015@gmail.com>
2025-07-12 14:54:00 +0700

    update

--- src/routes/web/web.js
+++ src/routes/web/web.js
@@ -3,14 +3,6 @@ const userWebRoutes = require('./user.route');
 const roleWebRoutes = require('./role.route');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('home', { title: 'Home Page', name: 'Hiep' });
-});
-
-router.get('/about', (req, res) => {
-    res.render('about', { title: 'About Us' });
-});
-
 router.get('/dashboard', (req, res) => {
     res.render('dashboard', { title: 'Dashboard' });
 });
